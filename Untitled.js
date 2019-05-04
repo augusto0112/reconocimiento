b17c716e91078aac5829ad760e4e3d240f2d5271
augusto0112 <cristianrestrepo96@hotmail.com>
2019-05-03 19:14:35 -0500

    example4 v1

---
+++ Untitled.js
@@ -0,0 +1,12 @@
+db.habitante.insertOne(
+{
+    cedula:1107093927,
+    nombres:"Cristian Augusto",
+    apellidos:"Restrepo Lenis",
+    numeroApto:"401B",
+    fechaRegistro:"02/05/2019-07:32:00",
+    ultimoIngreso:"02/05/2019-07:32:00",
+    primeraFoto:"img/1107093927-1.jpg",
+    segundaFoto:"img/1107093927-2.jpg",
+    terceraFoto:"img/1107093927-3.jpg"
+})
---
+++ db.py
@@ -0,0 +1,22 @@
+import pymongo
+from pymongo import MongoClient
+from pprint import pprint
+import bson
+import sys
+
+#Se establece la conección con autenticación
+#Cliente
+client = MongoClient(
+    "mongodb://localhost:27017/",
+    username="admin",
+    password="admin123",
+    authSource="admin",
+    authMechanism="SCRAM-SHA-256")
+#Base de datos
+db = client["unidadResidencialTB"]
+#Tabla
+habitante = db["habitante"].find( {"cedula":1107093927} )
+
+#Bucar e imprimir
+for busqueda in habitante:
+    pprint(busqueda)
